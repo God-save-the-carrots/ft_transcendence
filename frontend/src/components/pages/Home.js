0368eb04416c7d9bbd08a38816fe995ae3a945b5
bestjinam <jinam@studetn.42seoul.kr>
2024-01-22 15:31:06 +0900

    구조 변경 완료

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import Component from "../../core/Component.js";
 import Test_app1 from "../test/Test_app1.js";
 import Test_app2 from "../test/Test_app2.js";
+import Test_app3 from "../test/Test_app3.js";
 
 export default class Home extends Component {
 	_title;
@@ -15,17 +16,22 @@ export default class Home extends Component {
     		<h1> ${this._title} </h1>
     		<main data-component="test-app1"></main>
     		<main data-component="test-app2"></main>
+    		<main data-component="test-app3"></main>
     	`;
 	}
 	mounted() {
 		const _test_app1 = this.$target.querySelector(
-			'[data-component="test-app1"]'
+			'[data-component="test-app1"]',
 		);
 		const _test_app2 = this.$target.querySelector(
-			'[data-component="test-app2"]'
+			'[data-component="test-app2"]',
+		);
+		const _test_app3 = this.$target.querySelector(
+			'[data-component="test-app3"]',
 		);
 
 		new Test_app1(_test_app1);
 		new Test_app2(_test_app2);
+		new Test_app3(_test_app3);
 	}
 }
--- frontend/src/core/Component.js
+++ frontend/src/core/Component.js
@@ -1,30 +1,53 @@
+import { observable, observe } from "./observer.js";
+
 export default class Component {
 	$target;
-	//props;
-	state;
+	// props;
+	state = {};
 	constructor($target) {
 		this.$target = $target;
-		//this.props = props;
+		// this.props = props;
 		this.setup();
-		this.setEvent();
-		//this.render();
+		// this.setEvent();
+		// this.render();
 	}
-	setup() {}
+
+	setup() {
+		// this.state = observable(this.initState());
+		this.state = observable(this.initState());
+		observe(() => {
+			this.render();
+			this.setEvent();
+			this.mounted();
+		});
+	}
+
+	initState() {
+		return {};
+	}
 	mounted() {}
+
 	template() {
 		return "";
 	}
+
 	render() {
+		this.clearEvent();
 		this.$target.innerHTML = this.template();
+		this.setEvent();
 		this.mounted();
 	}
+
 	setEvent() {}
-	setState(newState) {
-		this.state = { ...this.state, ...newState };
-		this.render();
-	}
+	// setState(newState) {
+	// 	this.state = { ...this.state, ...newState };
+	// 	this.render();
+	// }
+	//
+	clearEvent() {}
+
 	addEvent(eventType, selector, callback) {
-		const children = [...this.$target.querySelectorAll(selector)];
+		//const children = [...this.$target.querySelectorAll(selector)]
 		this.$target.addEventListener(eventType, (event) => {
 			if (!event.target.closest(selector)) return false;
 			callback(event);
