92892ab257bbf0ecc821c4f135e749b475c55724
bestjinam <jinam@studetn.42seoul.kr>
2024-01-24 16:14:46 +0900

    consolog 정리, 다른 브랜치로 추가할 코드 삭제

--- frontend/src/core/observer.js
+++ frontend/src/core/observer.js
@@ -3,15 +3,9 @@ let currentObserver = null;
 function debounceFrame(callback) {
 	let currentCallback = -1;
 	return () => {
-		// cancelAnimationFrame(currentCallback);
-		console.log("sakdas");
+		cancelAnimationFrame(currentCallback);
 		currentCallback = requestAnimationFrame(callback);
 	};
-}
-
-function isSameObject(obj1, obj2) {
-	if (obj1 instanceof Set || obj2 instanceof Set) return false;
-	if (JSON.stringify(obj1) === JSON.stringify(obj2)) return true;
 }
 
 export async function observe(fn) {
