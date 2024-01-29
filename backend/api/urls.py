266a8ad987d15ef855837402e6c56e8d9afadfd6
junmokang <50203674+moaoh@users.noreply.github.com>
2024-01-29 21:36:02 +0900

    feat: backend - add score and matches dummy data (#61)
    * feat: backend - add score and matches dummy data
    
    * fix: :bug: dummy data 케이스 추가

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 
-from .views import LoginAPIView, LogoutAPIView, UserAPIView
+from .views import LoginAPIView, LogoutAPIView, UserAPIView, SocreAPIView, MatchesAPIView
 
 urlpatterns = [
     path('login', LoginAPIView.as_view()),
     path('logout', LogoutAPIView.as_view()),
     path('user/<str:intra_id>/', UserAPIView.as_view()),
+    path('game/phong/score/<str:intra_id>/', SocreAPIView.as_view()),
+    path('game/phong/matches/<int:match_id>/', MatchesAPIView.as_view()),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -64,3 +64,78 @@ class UserAPIView(APIView):
         if 'message' in request_data:
             response_data['message'] = request_data['message']
         return Response(response_data, status=status.HTTP_200_OK)
+
+class SocreAPIView(APIView):
+    dummy = {
+        'page': None,
+        'page_size': None,
+        'last_page_index': 2,
+        'content_length': 20,
+        'data': []
+    }
+    defult_dummy = []
+
+    def __init__(self):
+        for i in range(11):
+            dummy_entry = {
+                'match_id': i + 1,
+                'game_type': 'phong_4',
+                'score': [
+                    { 'user': {'id': (i * 3 + 1), 'intra_id': f'main_dummy', 'photo_id': i % 9},  'value': 100 },
+                    { 'user': {'id': (i * 3 + 4), 'intra_id': f'dummy-{i * 3 + 1}', 'photo_id': (i + 1) % 9}, 'value': -60 },
+                    { 'user': {'id': (i * 3 + 3), 'intra_id': f'dummy-{i * 3 + 2}', 'photo_id': (i + 2) % 9}, 'value': -40 },
+                    { 'user': {'id': (i * 3 + 5), 'intra_id': f'dummy-{i * 3 + 3}', 'photo_id': (i + 3) % 9}, 'value': -20 },
+                ]
+            }
+            self.defult_dummy.append(dummy_entry)
+        self.dummy['data'] = self.defult_dummy
+
+
+    def get(self, request, intra_id):
+        game_type = request.query_params.get('game_type', None)
+        intra_id = request.query_params.get('intra_id', None)
+        page = int(request.query_params.get('page', 0))
+        page_size = int(request.query_params.get('page_size', 10))
+
+        response_data = self.dummy
+        response_data['page'] = page
+        response_data['page_size'] = page_size
+        return Response(response_data, status=status.HTTP_200_OK)
+    
+class MatchesAPIView(APIView):
+    dummy = []
+    def __init__(self):
+        if not self.dummy:
+            for i in range(11):
+                self.dummy.append({
+                    "match_id": i + 1,
+                    "game_type": "phong_4",
+                    "score": [
+                        {
+                            "user": { "id": 1, "intra_id": "main_dummy-" + str(i), "photo_id": 0 },
+                            "rating": 1024,
+                            "value": 100
+                        },
+                        {
+                            "user": { "id": 4, "intra_id": "dummy", "photo_id": 0 },
+                            "rating": 900,
+                            "value": -20
+                        },
+                        {
+                            "user": { "id": 3, "intra_id": "dummy", "photo_id": 0 },
+                            "rating": 1100,
+                            "value": -40
+                        },
+                        {
+                            "user": { "id": 2, "intra_id": "dummy", "photo_id": 0 },
+                            "rating": 1032,
+                            "value": -60
+                        },
+                    ],
+                })
+
+    def get(self, request, match_id):
+        response_data = next((item for item in self.dummy if item['match_id'] == match_id), None)
+        if not response_data:
+            return Response(response_data, status=status.HTTP_404_NOT_FOUND)
+        return Response(response_data, status=status.HTTP_200_OK)
