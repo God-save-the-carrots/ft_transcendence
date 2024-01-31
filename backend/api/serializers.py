af72ef07be2e71ee6f9f23cbaea317b6c11ca757
moaoh <k010103@naver.com>
2024-01-31 23:43:46 +0900

    feat: :zap: User model 및 Profile model을 json 형식으로 변경해주는 내용추가

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,4 +1,17 @@
+from django.apps import apps
 from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import User, Profile
 
 class LoginSerializer(serializers.Serializer):
     code = serializers.CharField(max_length=100)
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
