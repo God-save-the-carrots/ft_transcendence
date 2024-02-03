ffa52dcfe8b69448b8d76a1a9c9c011213cc6168
yonshin <yonshin@student.42seoul.kr>
2024-02-04 01:40:15 +0900

    feat: text

--- frontend/src/game/common/Text.js
+++ frontend/src/game/common/Text.js
@@ -1,13 +1,11 @@
+import * as THREE from "../../threejs/three.js";
 import { TextGeometry } from "../../threejs/geometries/TextGeometry.js";
 import { FontLoader } from "../../threejs/loaders/FontLoader.js";
-import * as THREE from "../../threejs/three.js";
 import { GameObject } from "../GameObject.js";
 
 const loader = new FontLoader();
-let font;
-loader.load("/src/threejs/fonts/optimer_regular.typeface.json", function ( response ) {
-    font = response;
-} );
+let initload = false;
+let font = null;
 
 export default class Text extends GameObject {
     constructor(params) {
@@ -15,25 +13,57 @@ export default class Text extends GameObject {
             color: 0xffffff,
             position: { x: 0, y: 0, z: 0 },
             text: "hello threejs",
+            refreshCallback: () => {},
             ...params,
         }
+        super(new THREE.BufferGeometry(), new THREE.MeshBasicMaterial());
+        this.params = params;
+        this.position.set(params.position.x, params.position.y, params.position.z)
+
+        this.firstFrame = false;
+        if (font) this.refresh();
+        this.loadfont();
+    }
+
+    loadfont() {
+        if (initload) return;
+        initload = true;
+        loader.load("/src/threejs/fonts/optimer_regular.typeface.json", x => {
+            font = x;
+        });
+    }
+    update() {
+        if (font == null || this.firstFrame) return;
+        this.refresh();
+        this.firstFrame = true;
+    }
+    refresh() {
+        if (this.textObject) {
+            this.remove(this.textObject);
+        }
+        this.textObject = this.createText(this.params);
+        this.add(this.textObject);
+        this.params.refreshCallback(this.textObject.geometry.boundingBox);
+    }
+    createText(params) {
         const materials = [
-            new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true } ), // front
-            new THREE.MeshPhongMaterial( { color: 0xffffff } ) // side
+            new THREE.MeshPhongMaterial( { color: params.color, flatShading: true } ), // front
+            new THREE.MeshPhongMaterial( { color: params.color } ) // side
         ];
         const geometry = new TextGeometry(params.text, {
             font,
-            size: 0.1,
-            height: 1,
-            curveSegments: 100,
-
-            bevelThickness: 1,
-            bevelSize: 5,
+            size: 5,
+            height: 0.1,
+            curveSegments: 4,
+            bevelThickness: 0.1,
+            bevelSize: 0.2,
             bevelEnabled: true
         });
-        super(geometry, materials);
-        this.position.set(params.position.x, params.position.y, params.position.z);
-        this.rotation.x = 0;
-        this.rotation.y = Math.PI * 2;
+        geometry.computeBoundingBox();
+        const mesh = new THREE.Mesh(geometry, materials)
+        const width = geometry.boundingBox.max.x - geometry.boundingBox.min.x;
+        const height = geometry.boundingBox.max.y - geometry.boundingBox.min.y;
+        mesh.position.set(-width*0.5, -height*0.5, 0);
+        return mesh;
     }
 }
