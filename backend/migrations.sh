b6861c79d071907f34ca3748817cdce1c2677833
moaoh <k010103@naver.com>
2024-02-07 16:22:16 +0900

    feat: :zap: dummy data create 및 delete 하는 로직 추가

--- backend/migrations.sh
+++ backend/migrations.sh
@@ -2,3 +2,6 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py shell < tools/create_superuser.py
 python manage.py collectstatic --noinput
+
+python manage.py delete_dummy_data
+python manage.py create_dummy_data
