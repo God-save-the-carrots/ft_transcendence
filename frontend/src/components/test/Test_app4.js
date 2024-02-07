2755e6a572122c8a60d8672ea966f984c04a15c0
cheseo <devcseo@gmail.com>
2024-02-07 16:34:03 +0900

    Style/style guide (#70)
    * chore: :see_no_evil: update gitignore
    
    * chore: :wrench: update eslintrc
    
    * build: :wrench: add package.json for npm
    
    * style: :art: apply eslint
    
    google style

--- frontend/src/components/test/Test_app4.js
+++ frontend/src/components/test/Test_app4.js
@@ -1,22 +1,22 @@
-import Component from "../../core/Component.js";
+import Component from '../../core/Component.js';
 
 export default class Test_app4 extends Component {
-	_title;
-	constructor(target) {
-		super(target);
-		this._title = "item3";
-	}
-	async initState() {
-		const data = await fetch("https://fakestoreapi.com/products").then((x) =>
-			x.json()
-		);
-		return data[0];
-	}
-	async template() {
-		const data = this.state;
-		setTimeout(() => {
-			data.rating = { ...data.rating, rate: 333333 };
-		}, 2000);
-		return `<div>${JSON.stringify(data)}</div>`;
-	}
+  _title;
+  constructor(target) {
+    super(target);
+    this._title = 'item3';
+  }
+  async initState() {
+    const data = await fetch('https://fakestoreapi.com/products').then((x) =>
+      x.json(),
+    );
+    return data[0];
+  }
+  async template() {
+    const data = this.state;
+    setTimeout(() => {
+      data.rating = {...data.rating, rate: 333333};
+    }, 2000);
+    return `<div>${JSON.stringify(data)}</div>`;
+  }
 }
