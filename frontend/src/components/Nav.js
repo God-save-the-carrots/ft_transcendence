9abc49b3c29780dd0e715e0ebb7cfffee5e14a13
bestjinam <ggul_jam@icloud.com>
2024-02-08 22:55:09 +0900

    feat: navbar 새로고침 / color 적용/ 폰트 적용/ 스타일 적용 완료

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -3,84 +3,44 @@ import Router from '../core/Router.js';
 
 export default class Nav extends Component {
   async template() {
-    const afterLoginHtml = `
-      <link rel="stylesheet" type="text/css" 
-        href="../../public/assets/nav/nav.css" />
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-          <button class="navbar-toggler" type="button" 
-            data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01"
-            aria-controls="navbarTogglerDemo01" aria-expanded="false" 
-            aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <a class="navbar-brand" href="/" data-link></a>
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li class="nav-item" data-link>
-                <a class="nav-link active" aria-current="page" href="/game">
-                  <img src="../../public/assets/nav/game.png"data-link/>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/rank">
-                  <img src="../../public/assets/nav/crown.png" data-link/>
-                </a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="/user/dummy1" 
-                id="navbarDropdownMenuLink" role="button" 
-                data-bs-toggle="dropdown" aria-expanded="false">
-                  <img src="../../public/assets/nav/42logo.png"/>
-                </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <li><a class="dropdown-item" href="/user/dummy1" data-link>Profile
-              </a></li>
-              <li><a class="dropdown-item" href="#" data-link>Logout</a></li>
-              <li><a class="dropdown-item" href="/test" data-link>Test</a></li>
+    return `
+<link rel="stylesheet" type="text/css" href="../../public/assets/nav/nav.css" />
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <a class="navbar-brand" href="/" data-link></a>
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="/game" data-link>
+            <img src="../../public/assets/nav/game.png"/>
+            game
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/rank" data-link>
+            <img src="../../public/assets/nav/crown.png"/>
+            rank
+          </a>
+        </li>
+        <li class="dropdown nav-item">
+          <a class="nav-link " href="/user/dummy1" 
+            id="navbarDropdownMenuLink" role="button"
+            data-bs-toggle="dropdown" aria-expanded="false">
+            <img class="nav-profile" src="../../public/assets/nav/42logo.png"/>
+          </a>
+          <ul class="dropdown-menu dropdown-menu-lg-end" 
+            aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="/user/dummy1" data-link>
+              Profile</a></li>
+            <li><a class="dropdown-item" href="/home" data-link>Logout</a></li>
+            <li><a class="dropdown-item" href="/test" data-link>Test</a></li>
             </ul>
-            </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    `;
-    const beforeLoginHtml = `
-      <link rel="stylesheet" type="text/css" 
-        href="../../public/assets/nav/nav.css" />
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-          <button class="navbar-toggler" type="button" 
-            data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01"
-            aria-controls="navbarTogglerDemo01" aria-expanded="false" 
-            aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <a class="navbar-brand" href="/" data-link></a>
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li class="nav-item" data-link>
-                <a class="nav-link active" aria-current="page" href="/game">
-                  <img src="../../public/assets/nav/heart.png"data-link/>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/rank">
-                  <img src="../../public/assets/nav/heart.png" data-link/>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/rank">
-                  <img src="../../public/assets/nav/heart.png" data-link/>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    `;
-    // if login
-    return afterLoginHtml;
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+`;
   }
   setEvent() {
     this.addEvent('click', '[data-link]', async (e) => {
@@ -92,3 +52,4 @@ export default class Nav extends Component {
     });
   }
 }
+
