0d3fca0cee3d1f4d6288ab10dc30de4a69d95e1a
bestjinam <ggul_jam@icloud.com>
2024-02-08 15:54:47 +0900

    Navbar 골조 완성

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -4,14 +4,41 @@ import Router from '../core/Router.js';
 export default class Nav extends Component {
   async template() {
     return `
-      <a href="/" class="nav__link" data-link>home</a>
-      <a href="/user/dummy1" class="nav__link" data-link>user-dummy</a>
-      <a href="/game" class="nav__link" data-link>game</a>
-      <a href="/rank" class="nav__link" data-link>ranks</a>
-      <a href="/test" class="nav__link" data-link>test</a>
-      <a href="/login" class="nav__link" data-link>login-test</a>
-      <input type="button" value="login" />
-    `;
+      <link rel="stylesheet" type="text/css" href="../../public/assets/nav/nav.css" />
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="/"></a>
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/game">
+                  <img src="../../public/assets/nav/game.png"/>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/rank">
+                  <img src="../../public/assets/nav/crown.png"/>
+                </a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="/user/dummy1" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  <img src="../../public/assets/nav/42logo.png"/>
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <li><a class="dropdown-item" href="/user/dummy1">Profile</a></li>
+                  <li><a class="dropdown-item" href="#">Logout</a></li>
+                  <li><a class="dropdown-item" href="/test">Test</a></li>
+                  </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+`;
   }
   setEvent() {
     this.addEvent('click', '[data-link]', async (e) => {
