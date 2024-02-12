8d3a2f06907f834339556c6a936d72b178f59930
veggie-garden <devcseo@gmail.com>
2024-02-12 18:45:04 +0900

    feat: :sparkles: change font, change logo png to text, rm useless margin padding

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -8,27 +8,25 @@ export default class Nav extends Component {
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
     <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <a class="navbar-brand" href="/" data-link></a>
+      <a class="navbar-brand" href="/" data-link>LIBFT</a>
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link" href="/game" data-link>
             <img src="../../public/assets/nav/game.png"/>
-            game
           </a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/rank" data-link>
             <img src="../../public/assets/nav/crown.png"/>
-            rank
           </a>
         </li>
         <li class="dropdown nav-item">
-          <a class="nav-link " href="/user/dummy1" 
+          <a class="nav-link " href="/user/dummy1"
             id="navbarDropdownMenuLink" role="button"
             data-bs-toggle="dropdown" aria-expanded="false">
             <img class="nav-profile" src="../../public/assets/nav/42logo.png"/>
           </a>
-          <ul class="dropdown-menu dropdown-menu-lg-end" 
+          <ul class="dropdown-menu dropdown-menu-lg-end"
             aria-labelledby="navbarDropdownMenuLink">
             <li><a class="dropdown-item" href="/user/dummy1" data-link>
               Profile</a></li>
