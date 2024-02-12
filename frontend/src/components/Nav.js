caae293834291d0f0e1dcd992898219b8a1cc5f2
42JINAM <158562186+42JINAM@users.noreply.github.com>
2024-02-12 20:51:36 +0900

    Feat/fe add new navbar (#74)
    * index.html에 bootstrap 을 추가 해두었고, 네비바 아이콘 추가함
    
    * User history 리스트를 출력 하도록 함
    
    test
    
    * remove bootrap css and js
    
    * Navbar 골조 완성
    
    * 새로고침 해결
    
    * login 전/후로 다른 navbar html 작성
    
    * fix : eslint
    에 따라 html 코드 수정
    
    * feat: navbar 새로고침 / color 적용/ 폰트 적용/ 스타일 적용 완료
    
    * index.html : bootstrap js 위치변경
    
    * Apply suggestions from code review
    
    Co-authored-by: cheseo <devcseo@gmail.com>
    
    * nav css 옮김
    
    ---------
    
    Co-authored-by: bestjinam <ggul_jam@icloud.com>
    Co-authored-by: cheseo <devcseo@gmail.com>

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -4,14 +4,43 @@ import Router from '../core/Router.js';
 export default class Nav extends Component {
   async template() {
     return `
-      <a href="/" class="nav__link" data-link>home</a>
-      <a href="/user/dummy1" class="nav__link" data-link>user-dummy</a>
-      <a href="/game" class="nav__link" data-link>game</a>
-      <a href="/rank" class="nav__link" data-link>ranks</a>
-      <a href="/test" class="nav__link" data-link>test</a>
-      <a href="/login" class="nav__link" data-link>login-test</a>
-      <input type="button" value="login" />
-    `;
+<link rel="stylesheet" type="text/css" href="../../public/assets/css/nav.css" />
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <a class="navbar-brand" href="/" data-link></a>
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="/game" data-link>
+            <img src="../../public/assets/nav/game.png"/>
+            game
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/rank" data-link>
+            <img src="../../public/assets/nav/crown.png"/>
+            rank
+          </a>
+        </li>
+        <li class="dropdown nav-item">
+          <a class="nav-link " href="/user/dummy1" 
+            id="navbarDropdownMenuLink" role="button"
+            data-bs-toggle="dropdown" aria-expanded="false">
+            <img class="nav-profile" src="../../public/assets/nav/42logo.png"/>
+          </a>
+          <ul class="dropdown-menu dropdown-menu-lg-end" 
+            aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="/user/dummy1" data-link>
+              Profile</a></li>
+            <li><a class="dropdown-item" href="/home" data-link>Logout</a></li>
+            <li><a class="dropdown-item" href="/test" data-link>Test</a></li>
+            </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+`;
   }
   setEvent() {
     this.addEvent('click', '[data-link]', async (e) => {
@@ -23,3 +52,4 @@ export default class Nav extends Component {
     });
   }
 }
+
