5a912e4f3b357cb60d72ea5d7467e1973129e860
junmokang <50203674+moaoh@users.noreply.github.com>
2024-02-13 10:35:10 +0900

    Feat/be add api (#75)
    * feat: :zap: game/pong/score/<str:intra_id>/profile - 유저 정보 및 rank 반환해주는 api 추가
    
    feat: :construction: working..
    
    * fix: :bug: query string type error 수정.
    
    * feat: :zap: dummy data에 start_time, end_time 추가
    
    * feat: :zap: api/game/pong/score/<str:intra_id>/play-time - play-time api 추가
    
    * refactor: :recycle: play-time APIView refactoring
    
    * feat: :zap: Pagination 처리 추가
    
    * feat: :zap: game/pong/score/<str:intra_id>/winning-rate - user에 경기 횟수 및 승률 api 추가
    
    * fix: :bug: 잘못된 요청이 들어온 경우 error 처리 추가
    
    * feat: :zap: api/game/pong/score/<str:intra_id>/goals-against-average - 내가 낸 점수와 상대가 낸 점수를 더한 결과를 반환해주는 api 추가.
    
    * feat: :zap: game/pong/score/<str:intra_id>/winning-percentage - intra_id winning, average winning, highest winning and  ScoreWinningRateAPIView refactoring

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path, include
+from django.urls import path
 
-from .views import LoginAPIView, LogoutAPIView, UserAPIView, ScoreAPIView, MatchesAPIView, RankAPIView
+from .views import *
 
 urlpatterns = [
     path('login', LoginAPIView.as_view()),
@@ -9,4 +9,9 @@ urlpatterns = [
     path('game/pong/rank/', RankAPIView.as_view()),
     path('game/pong/score/<str:intra_id>/', ScoreAPIView.as_view()),
     path('game/pong/matches/<int:match_id>/', MatchesAPIView.as_view()),
+    path('game/pong/score/<str:intra_id>/profile/', ScoreProfileAPIView.as_view()),
+    path('game/pong/score/<str:intra_id>/play-time/', ScorePlayTimeAPIView.as_view()),
+    path('game/pong/score/<str:intra_id>/winning-rate', ScoreWinningRateAPIView.as_view()),
+    path('game/pong/score/<str:intra_id>/goals-against-average', ScoreGoalsAgainstAverageAPIView.as_view()),
+    path('game/pong/score/<str:intra_id>/winning-percentage', ScoreWinningPercentageAPIView.as_view()),
 ]
