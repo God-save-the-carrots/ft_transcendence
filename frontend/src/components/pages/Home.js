ffdc6310e4cd9e49076499ef5f977b9ddd2c5f98
veggie-garden <devcseo@gmail.com>
2024-02-13 19:53:16 +0900

    feat: :sparkles: add css and html in js file

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -10,7 +10,25 @@ export default class Home extends Component {
   }
   async template() {
     return `
-      <h1> ${this._title} </h1>
+<link rel="stylesheet" type="text/css" href="../../public/assets/css/home.css">
+<div class="background">
+    <img class="map" src="../../public/assets/image/map.svg" alt="map">
+    <div class="content">
+      <div class="img-wrap">
+        <img src="../../public/assets/image/monster.svg" alt="" class="one">
+        <img src="../../public/assets/image/star.svg" alt="" class="two">
+        <img src="../../public/assets/image/monster.svg" alt="" class="three">
+      </div>
+      <p>
+        <span>Welcome</span>
+        <span>to</span>
+        <span class="title">LIBFT!</span>
+      </p>
+      <div class="btn-wrap">
+        <button class="login-btn">42 login</button>
+      </div>
+    </div>
+  </div>
     `;
   }
   async mounted() {}
