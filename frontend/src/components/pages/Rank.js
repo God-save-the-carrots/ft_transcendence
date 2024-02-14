cf93eaba0c5fe79a782d3b48c3dd5353c4225162
veggie-garden <devcseo@gmail.com>
2024-02-14 21:53:39 +0900

    feat: :sparkles: making rank connect with user data

--- frontend/src/components/pages/Rank.js
+++ frontend/src/components/pages/Rank.js
@@ -3,11 +3,19 @@ import Component from '../../core/Component.js';
 export default class Rank extends Component {
   _title;
   _params;
-  constructor() {
+  constructor(params = null) {
     super(document.querySelector('#app'));
     this._title = 'Rank';
+    this._params = params;
   }
+
   async template() {
+    const profile_api = `http://localhost/api/user/${this._params.intra_id}`;
+    this.state.data = await fetch(profile_api,
+    ).then((x) => x.json());
+    const data = this.state.data;
+    const img = `/public/assets/profile/${data.user.photo_id}.png`;
+    const intra_id = `${this._params.intra_id}`;
     return `
     <link rel="stylesheet" href="../../../public/assets/css/rank.css">
     <div class="block-wrap">
@@ -17,9 +25,9 @@ export default class Rank extends Component {
           <span class="rank">1</span>
           <div class="space"></div>
           <span class="profile">
-            <img src="../../../public/assets/profile/2.png" alt="profile-image">
+            <img src="${img}" alt="profile-image">
           </span>
-          <span class="username">cheseo</span>
+          <span class="username">${intra_id}</span>
           <span class="msg">나는 당근~🥕</span>
           <span class="score">100</span>
         </li>
@@ -80,4 +88,5 @@ export default class Rank extends Component {
   </div>
       `;
   }
+  async mounted() {}
 }
