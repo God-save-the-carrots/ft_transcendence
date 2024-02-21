ea1d630e8fa3adf71e925dddc4db2e675b57881f
veggie-garden <devcseo@gmail.com>
2024-02-21 20:04:09 +0900

    feat: :sparkles: making login feature

---
+++ frontend/src/components/pages/Auth.js
@@ -0,0 +1,29 @@
+import Component from '../../core/Component.js';
+
+export default class Auth extends Component {
+  _title;
+  _params;
+  constructor(params = null) {
+    super(document.querySelector('#app'));
+    this._title = 'Auth';
+    this._params = params;
+  }
+  async template() {
+    const auth_api = window.location.href;
+    const code = auth_api.split('code=');
+    const login_api = `http://localhost/api/login`;
+
+    console.log(auth_api);
+    fetch(login_api, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({code}),
+    })
+        .then((x) => x.json())
+        .then((data) => console.log(data));
+    return ``;
+  }
+  async mounted() {}
+}
--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -12,24 +12,35 @@ export default class Home extends Component {
     return `
 <link rel="stylesheet" type="text/css" href="../../public/assets/css/home.css">
 <div class="background">
-    <img class="map" src="../../public/assets/image/map.svg" alt="map">
+    <img class="map" src="../../public/assets/image/map.svg" alt="map"
+      draggable="false">
     <div class="content">
       <div class="img-wrap">
-        <img src="../../public/assets/image/monster.svg" alt="" class="one">
-        <img src="../../public/assets/image/star.svg" alt="" class="two">
-        <img src="../../public/assets/image/monster.svg" alt="" class="three">
+        <img src="../../public/assets/image/monster.svg" alt=""
+          class="one" draggable="false">
+        <img src="../../public/assets/image/star.svg" alt=""
+          class="two" draggable="false">
+        <img src="../../public/assets/image/monster.svg" alt=""
+          class="three" draggable="false">
       </div>
       <p>
         <span>Welcome</span>
         <span>to</span>
         <span class="title">LIBFT!</span>
       </p>
-      <div class="btn-wrap">
-        <button class="login-btn">42 login</button>
-      </div>
+      <button class="login-btn" login>42 login</button>
     </div>
   </div>
     `;
   }
   async mounted() {}
+
+  setEvent() {
+    this.addEvent('click', '[login]', async (e) => {
+      const client_id = 'u-s4t2ud-fdd6b022ac73f838ea32e530b384e8142cdde216bc237e60bbe6ab0c2d527087';
+      const redirect_uri = 'http://localhost/auth/ft/redirection';
+      const login_api = `https://api.intra.42.fr/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code`;
+      location.href = login_api;
+    });
+  }
 }
