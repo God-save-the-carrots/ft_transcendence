9bf8656e8b242264a9b4f9e5382c4589506e8d9e
veggie-garden <devcseo@gmail.com>
2024-02-23 19:48:28 +0900

    feat: :sparkles: create cookie and redirect to /rank page if login successful
    추후 rank 페이지가 아닌 자기 프로필로 갈 수 있게 변경해야 합니다

--- frontend/src/components/pages/Auth.js
+++ frontend/src/components/pages/Auth.js
@@ -1,4 +1,5 @@
 import Component from '../../core/Component.js';
+import Router from '../../core/Router.js';
 
 export default class Auth extends Component {
   _title;
@@ -22,7 +23,7 @@ export default class Auth extends Component {
       body: JSON.stringify({code}),
     })
         .then((x) => x.json())
-        .then((data) => console.log(data));
+        .then((data) => checkCookie(data));
     return ``;
   }
   async mounted() {}
@@ -31,15 +32,18 @@ export default class Auth extends Component {
 function setCookie(name, value, options = {}) {
   options = {
     path: '/',
-    'max-age': '3600',
-    expires: 3600
+    expires: new Date(),
+    'max-age': new Date(),
+    life: 14 // day
   };
 
   if (options.expires instanceof Date) {
+    options.expires.setTime(options.expires.getTime() + (options.life * 24 * 60 * 60 * 1000));
     options.expires = options.expires.toUTCString();
   }
   if (options['max-age'] instanceof Date) {
-    options['max-age'] = options.expires.toUTCString();
+    options['max-age'].setTime(options['max-age'].getTime() + (options.life * 24 * 60 * 60 * 1000));
+    options['max-age'] = options['max-age'].toUTCString();
   }
 
   let updatedCookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
@@ -55,10 +59,53 @@ function setCookie(name, value, options = {}) {
   document.cookie = updatedCookie;
 }
 
+export function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function deleteCookie() {
+  setCookie('access', "", {
+    'max-age': -1,
+    expires: -1
+  });
+  setCookie('refresh', "", {
+    'max-age': -1,
+    expires: -1
+  });
+}
+
 function setToken(data) {
   const token_arr = Object.entries(data);
   const access = token_arr[0];
   const refresh = token_arr[1];
   setCookie(access[0], access[1]);
   setCookie(refresh[0], refresh[1]);
+}
+
+function checkCookie(data) {
+  let access = getCookie('access');
+  let redirection_uri = 'http://localhost/';
+  if (access === undefined) {
+    console.log('set cookie');
+    setToken(data);
+  }
+  access = getCookie('access');
+  const uri = `http://localhost:8000/api/game/pong/rank/`;
+  fetch(uri, {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${access}`,
+    }
+  })
+    .then(function(response) {
+      if (response.ok) {
+        console.log('response ok', response);
+        redirection_uri = `http://localhost/rank/`;
+        //Response.redirect(redirection_uri);
+        Router.navigateTo('/rank');
+      }
+    })
 }
