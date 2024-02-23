744a99c505e44ec325b2dd4ea2d327f7ae78890f
moaoh <k010103@naver.com>
2024-02-24 02:29:09 +0900

    refactor: :recycle: 'token/refresh/' -> 'token/verify/'로 변경.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,5 +5,5 @@ from .views import *
 urlpatterns = [
     path('', include('accounts.urls')),
     path('game/pong/', include('pong.urls')),
-    path('token/refresh/', TokenRefreshAPIView.as_view()),
+    path('token/verify/', TokenRefreshAPIView.as_view()),
 ]
