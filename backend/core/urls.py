e015861e492b8c2ff8c46ee90332b25382c062d1
moaoh <k010103@naver.com>
2024-02-24 22:32:46 +0900

    feat: :construction: working.. prometheus

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('api/', include('api.urls')),
-    path('monitor/', include('django_prometheus.urls')),
+    path('', include('django_prometheus.urls')),
 ]
