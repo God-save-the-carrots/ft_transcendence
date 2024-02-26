880aa7c6afa75e11e36590f5e480bb624f4d6dda
veggie-garden <devcseo@gmail.com>
2024-02-26 19:06:18 +0900

    feat: :recycle: update frontend env

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,16 +1,21 @@
 import Component from '../../core/Component.js';
+import {pubEnv} from '../../const.js';
+
+const endpoint = pubEnv.API_SERVER;
 
 export default class Home extends Component {
   _title;
   _params;
+  _my_css;
   constructor(params = null) {
     super(document.querySelector('#app'));
     this._title = 'Home';
     this._params = params;
+    this._my_css = '../../public/assets/css/home.css';
   }
   async template() {
     return `
-<link rel="stylesheet" type="text/css" href="../../public/assets/css/home.css">
+<link rel="stylesheet" href="${this._my_css}" type="text/css">
 <div class="background">
     <img class="map" src="../../public/assets/image/map.svg" alt="map"
       draggable="false">
@@ -37,8 +42,9 @@ export default class Home extends Component {
 
   setEvent() {
     this.addEvent('click', '[login]', async (e) => {
-      const client_id = 'u-s4t2ud-63528798c2d1ca6b3471b90037fb70e93374cd3039832416be112b1fa70fa6b3';
-      const redirect_uri = 'http://localhost/auth/ft/redirection';
+      const client_id = `${pubEnv.API_KEY}`;
+      const redirect_uri = `http://localhost/auth/ft/redirection`;
+      // const redirect_uri = `${endpoint}/auth/ft/redirection`;
       const login_api = `https://api.intra.42.fr/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code`;
       location.href = login_api;
     });
--- frontend/src/const.js
+++ frontend/src/const.js
@@ -1,3 +1,7 @@
 export const pubEnv = {
   API_SERVER: 'https://localhost',
+  API_KEY:
+    'u-s4t2ud-63528798c2d1ca6b3471b90037fb70e93374cd3039832416be112b1fa70fa6b3',
+  TOKEN_ACCESS: 'access',
+  TOKEN_REFRESH: 'refresh',
 };
