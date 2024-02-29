f3ff649df917441fa15a4b8396345100e9242a54
moaoh <k010103@naver.com>
2024-02-29 20:51:26 +0900

    feat: :zap: csrf off..

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -31,8 +31,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-# CSRF 잠시 무효화
-CSRF_TRUSTED_ORIGINS = ['http://localhost:80', "https://api.intra.42.fr"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost",  # 또는 프론트엔드 호스트의 도메인
@@ -66,7 +64,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
