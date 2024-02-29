40fe436cbd5c007604ff8669d6badb62ccbad8d2
yonshin <yonshin@student.42seoul.kr>
2024-02-29 18:22:47 +0900

    test: delete dummy

--- backend/migrations.sh
+++ backend/migrations.sh
@@ -2,6 +2,3 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py shell < tools/create_superuser.py
 python manage.py collectstatic --noinput
-
-python manage.py delete_dummy_data
-python manage.py create_dummy_data
