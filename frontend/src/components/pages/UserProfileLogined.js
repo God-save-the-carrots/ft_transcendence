91a8774f3a4d37d762114ba0064ddefa2a5fb758
42JINAM <jinam.42seoul@gmail.com>
2024-02-29 23:27:02 +0900

    feat: 네비바 재생성 막음

--- frontend/src/components/pages/UserProfileLogined.js
+++ frontend/src/components/pages/UserProfileLogined.js
@@ -38,7 +38,7 @@ export default class UserProfile extends Component {
             throw new Error();
           }
           this.state.photo_id = photo_id;
-          new Nav(document.querySelector('#nav'));
+          Nav.render();
         });
     // msg
     this.$target.querySelector('.msg-form')
